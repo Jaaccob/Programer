d6f1fd25488000cb15382fa84345858947c23a0d
Jaaccob <kozubekjacob@gmail.com>
2021-09-30 12:50:43 +0200

    Stworzenie abstrakcyjnej klasy Person

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public abstract class Person {
+    private String name;
+    private String lastName;
+    private String pesel;
+    private int id;
+
+    public Person(String name, String lastName, String pesel, int id) {
+        this.name = name;
+        this.lastName = lastName;
+        this.pesel = pesel;
+        this.id = id;
+    }
+
+    
+}
